87555310c24add660e4995d64afb553974de4ed6
oli360 <oliviermaxwell@me.com>
2020-12-15 14:20:04 +0100

    Initial commit

---
+++ Introduction_Statistical_Learning/Exercises/Currentexo.R
@@ -0,0 +1,18 @@
+graphics.off()          # clear the plots
+rm(list = ls())         # reset the workspace 
+setwd("~/R_Projects")   # set working directory
+
+college =read.csv("Introduction_Statistical_Learning/Given_Material/AISL_dataSets/College.csv")
+rownames(college)=college[,1]
+college=college[,-1]
+attach(college)
+
+#print summary of data
+cat("Summary of data: \n")
+print(summary(college))
+
+pairs(college[,2:10]) #get glimpse into data
+
+Private=as.factor(Private) # change Private to qualitative
+plot(Private, Outstate, xlab="Private", ylab="Out-of-state")    # plot 
+
